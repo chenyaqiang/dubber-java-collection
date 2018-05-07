7f5ac786681a7d00e73f83a84e334211b4ba698d
dubber222 <17521010890@163.com>
2018-05-07 11:45:01 +0800

    Java 集合类 LinkedList

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        HashMap map = new HashMap();
+
         System.out.println("Hello World!");
     }
 }
